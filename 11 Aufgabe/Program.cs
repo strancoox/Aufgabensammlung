921a6c08f7d03b824d448fc05a3b5deb5d0bb5a0
Alex Stranc <aleksander_stranc@sluz.ch>
2024-10-02 15:17:17 +0200

    Aufgabe 11 fertig.

---
+++ 11 Aufgabe/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace _11_Aufgabe
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie lange soll die Linie sein?: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            DrawLine(input);
+
+            Console.ReadKey();
+        }
+
+        static void DrawLine(int number)
+        {
+
+            for (int row = 1; row <= number; row++)
+            {
+                for (int column = 1; column <= number; column++)
+                {
+                    if (row == column)
+                    {
+                        Console.Write(" ");             
+                    }
+                    else
+                    {
+                        Console.Write("* ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
