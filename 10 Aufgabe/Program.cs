b9f878621194198846e84a94d46257487eb996a9
Alex Stranc <aleksander_stranc@sluz.ch>
2024-10-02 14:08:16 +0200

    Aufgabe 10 fertig.

--- 10 Aufgabe/Program.cs
+++ 10 Aufgabe/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -13,22 +14,49 @@ namespace _10_Aufgabe
         {
             int sum = 0;
 
-            // Solange die Zahl nicht 0 ist, summiere die Ziffern
             while (zahl != 0)
             {
                 sum = sum + (zahl % 10);
                 zahl = zahl / 10;
             }
 
-            return sum; // Gib die berechnete Quersumme zurück
+            return sum; // Gib die berechnete Quersumme zurück für die erste Eingabe
+
         }
+        
         static void Main(string[] args)
             {
+
             Console.WriteLine("Zahl 1: ");
             string zahl1 = Console.ReadLine();
             Console.WriteLine("Zahl 2: ");
             string zahl2 = Console.ReadLine();
+
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Zahl\t" + "| Quersumme\t" + "| Zahl / Quersumme");
+            Console.WriteLine("------------------------------------------");
+
+            if(int.TryParse(zahl1, out int start) && int.TryParse(zahl2,out int end))
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    int quersumme = BerechneQuersumme(i);
+                    
+                    if (i % quersumme == 0)
+                    {
+                        Console.WriteLine(i + "\t|" + " " + quersumme + "\t\t|" + " " + i / quersumme);
+                    }
+                }
             }
+            else
+            {
+                Console.WriteLine("Ungültige Eingaben. Bitte geben Sie gültige Ganzzahl ein.");
+            }
+            Console.ReadKey();
         }
     }
+}
+
+
+
 
