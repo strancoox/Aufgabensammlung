ae3808396b2f9d053634f178741638d8c6d03f16
Alex Stranc <aleksander_stranc@sluz.ch>
2024-10-02 15:59:05 +0200

    Aufgabe 12 (fertig)

---
+++ 12 Aufgabe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Aufgabe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+
+            doSomething(); 
+            while (i >= 0)
+            {
+                doSomething(); 
+            }
+
+        }
+        static void doSomething()
+        {
+            Console.WriteLine("Hallo");
+        }
+    }
+}
